7a03760d76739d28a921d4bd050c520e43bc6bfc
Sambo Chea <sombochea@cubetiqs.com>
2020-12-20 15:09:20 +0700

    add openjdk 11 jre alpine custom build with fonts support

---
+++ 11/jre/alpine/buildAndPublish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REGISTRY=registry.osa.cubetiqs.com
+IMAGE_NAME=openjdk11
+TAG=jre-11u-alpine
+IMAGE=${REGISTRY}/${IMAGE_NAME}:${TAG}
+
+echo "Start building the image: ${IMAGE}"
+docker build . -t ${IMAGE}
+
+echo "Start pushing the image: ${IMAGE}"
+docker push ${IMAGE}
+
+echo "Build successfully!"
